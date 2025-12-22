33c8038e5de2e93fdd102bcfaac03057ac95225b
bunny123373 <korrasanhok@gmail.com>
2025-12-22 22:12:14 +0530

    fix: cloudinary poster & video play

--- public/app.js
+++ public/app.js
@@ -1,91 +1,48 @@
 const grid = document.getElementById("grid");
-const search = document.getElementById("search");
-const language = document.getElementById("language");
-const category = document.getElementById("category");
-
 const modal = document.getElementById("playerModal");
 const player = document.getElementById("player");
 const closeBtn = document.getElementById("close");
 
-const continueSection = document.getElementById("continueSection");
-const continueRow = document.getElementById("continueRow");
-
-let currentMovie = null;
-
-/* ---------- LOAD MOVIES ---------- */
-function load(){
-  let url = "/videos?";
-  if (search.value) url += "search=" + search.value + "&";
-  if (language.value) url += "language=" + language.value + "&";
-  if (category.value) url += "category=" + category.value;
-
-  fetch(url).then(r=>r.json()).then(show);
+function loadMovies(){
+  fetch("/videos")
+    .then(r => r.json())
+    .then(showMovies)
+    .catch(()=> {
+      grid.innerHTML = "<p style='color:white'>Cannot get videos</p>";
+    });
 }
 
-/* ---------- SHOW GRID ---------- */
-function show(list){
+function showMovies(list){
   grid.innerHTML = "";
-  list.forEach(v=>{
-    const div = document.createElement("div");
-    div.className = "card";
-    div.innerHTML = `
-      <div class="poster" style="background-image:url('/posters/${v.poster}')"></div>
-      <div class="title">${v.title}</div>
+
+  if(list.length === 0){
+    grid.innerHTML = "<p style='color:white'>No movies uploaded</p>";
+    return;
+  }
+
+  list.forEach(m => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <div class="poster" style="background-image:url('${m.posterUrl}')"></div>
+      <div class="title">${m.title}</div>
     `;
-    div.onclick = ()=>play(v);
-    grid.appendChild(div);
+
+    card.onclick = () => playMovie(m.videoUrl);
+    grid.appendChild(card);
   });
 }
 
-/* ---------- PLAY VIDEO ---------- */
-function play(movie){
-  currentMovie = movie;
-  player.src = "/stream/" + movie.video;
-  player.currentTime = localStorage.getItem(movie.id) || 0;
+function playMovie(url){
+  player.src = url;
   modal.style.display = "block";
+  player.play();
 }
 
-/* ---------- SAVE PROGRESS ---------- */
-player.ontimeupdate = ()=>{
-  if(currentMovie){
-    localStorage.setItem(currentMovie.id, player.currentTime);
-    localStorage.setItem("continue_"+currentMovie.id, JSON.stringify(currentMovie));
-    loadContinue();
-  }
-};
-
-closeBtn.onclick = ()=>{
+closeBtn.onclick = () => {
   player.pause();
   modal.style.display = "none";
 };
 
-/* ---------- CONTINUE WATCHING ---------- */
-function loadContinue(){
-  continueRow.innerHTML = "";
-  let found = false;
-
-  Object.keys(localStorage).forEach(k=>{
-    if(k.startsWith("continue_")){
-      const m = JSON.parse(localStorage.getItem(k));
-      found = true;
-      const div = document.createElement("div");
-      div.className = "card";
-      div.innerHTML = `
-        <div class="poster" style="background-image:url('/posters/${m.poster}')"></div>
-        <div class="title">${m.title}</div>
-      `;
-      div.onclick = ()=>play(m);
-      continueRow.appendChild(div);
-    }
-  });
-
-  continueSection.style.display = found ? "block" : "none";
-}
-
-/* ---------- EVENTS ---------- */
-search.oninput = load;
-language.onchange = load;
-category.onchange = load;
-
-load();
-loadContinue();
+loadMovies();
