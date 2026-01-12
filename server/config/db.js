799729f1264dda238b01daa553295a157c814c8d
bunny123373 <korrasanhok@gmail.com>
2026-01-12 09:14:08 +0530

    Add files via upload

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/baluflix");
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ DB Error", err);
+  }
+};
+
+export default connectDB;
